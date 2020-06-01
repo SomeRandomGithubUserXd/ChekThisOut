721e692d085db09713928815d357435e0114b240
SomeRandomGithubUserXd <62754822+SomeRandomGithubUserXd@users.noreply.github.com>
2020-06-01 15:01:26 +0300

    some

---
+++ anodapage/index.php
@@ -0,0 +1,94 @@
+<DOCTYPE html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <title>Project's Page</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style/style_two.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+        <meta name = "viewport" content = "width = device-width">
+        <meta name = "viewport" content = "initial-scale = 1.0">
+    </head>
+    <body>
+    <?php include ('addons/mh.php'); ?>
+    <div class="projects-menu">
+        <p class="wrap-menu">  <a href="/" class="projects-link-cur">Все проекты</a></p>
+        <p class="wrap-menu"><a href="/" class="projects-link">Жилые </a></p>
+        <p class="wrap-menu"> <a href="/" class="projects-link">Общественные здания</a></p>
+        <p class="wrap-menu"> <a href="/" class="projects-link">Коммерческая недвижимость</a></p>
+        <p class="wrap-menu"><a href="/" class="projects-link">Интерьеры </a></p>
+
+    </div>
+    <div class="projects">
+
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+        <span class="descr-img-bottom">Подробнее про Гуся</span>
+    </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+        <div class="image">
+        <img src="img/goose.png" class="project-img"></img>
+            <span class="descr-img-top">Название Гуся</span>
+            <span class="descr-img-center">Описание Гуся</span>
+            <span class="descr-img-bottom">Подробнее про гуся</span>
+        </div>
+
+
+    </div>
+    </body>
+</DOCTYPE>
